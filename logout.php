115b9392cc51f1b805cdee6759c3fe24c9363d29
Tawfiak22 <79513425+Tawfiak22@users.noreply.github.com>
2022-06-04 14:18:54 +0100

    logout

--- logout.php
+++ logout.php
@@ -1,19 +1,5 @@
-<!DOCTYPE html>
-<html>
-<body>
- 
 <?php
- 
-    echo "Logged out successfully";
- 
-    session_start();
-    if (session_destroy()) 
-    
-    {
-        header("location: index.php");
-    }
- 
+session_start();
+session_destroy();
+header("Location:index.php");
 ?>
- 
-</body>
-</html>
