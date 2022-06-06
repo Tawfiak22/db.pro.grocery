1737ba6882a1705fc899a9eec13e191227f16836
Tawfiak22 <79513425+Tawfiak22@users.noreply.github.com>
2022-06-06 23:46:56 +0100

    update

--- about.php
+++ about.php
@@ -28,24 +28,7 @@
       <div class="home-section" id="heading-section">
         <h1>Online Grocery Store</h1>
       </div>
-
-
-<!-- The H3 and H4 Section -->
-      <div class="home-section" id="home">
-        <h2> As we all know, 2020 was a very particular year for those who are lovers of music and not only, but also of the great live entertainment industry which unfortunately is still ongoing until the year 2021. 
-        We want to bring the experience of live music back to life, but in a very special way. 
-        What do you think of a virtual music festival directly from your home?
-        </h2>
-        <h3>If so, this is the right place for you!</h3>
-
-        <img src="home1.jpg" alt="home1" width=400px; height= 400px>
-        &#160
-        <img src="home2.jpg" alt="home2" width=400px; height= 400px>
-
-
-        <h2>Join us with friends and family!</h2>
-      </div>
-      
+  
 <!-- The Footer Section -->
 <footer>
   <p>Powered by me</p>
