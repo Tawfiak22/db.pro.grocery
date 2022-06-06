ad0241ecbfdf98de92d0fd03229ab6e34af3f49f
Tawfiak22 <79513425+Tawfiak22@users.noreply.github.com>
2022-06-06 18:22:06 +0100

    connection php

--- db.connection.php
+++ db.connection.php
@@ -14,13 +14,8 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-else{
-    echo "Connected successfully!";
-}
-
-//Close connection:
-$conn->close();
 ?>
 </body>
 </html>
+
 
