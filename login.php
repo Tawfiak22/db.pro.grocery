fd5d87e95bff5f678e3515de2e99d56a0498d315
Tawfiak22 <79513425+Tawfiak22@users.noreply.github.com>
2022-06-06 18:28:42 +0100

    login php

--- login.php
+++ login.php
@@ -1,87 +1,72 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="style.css"/>
-    <script src="form.js"></script>
-
-    <title>Online Grocery Store</title>
-
-  </head>
-
-  <body>
-    <nav>
-      <ul class="topnav">
-        <li><a class="active" href="index.html">HOME</a></li>
-        <li><a href="about.html">ABOUT</a></li>
-        <li><a href="product.html">PRODUCT</a></li>
-        <li><a href="faq.html">FAQ</a></li>
-        <li><a href="contact.html">CONTACT</a></li>
-        <li><a href="reg.html">SING UP</a></li>
-        <li><a href="log.php">LOG IN</a></li>
-        <li><a href="order.php">ORDER</a></li>
-      </ul>
-    </nav>
-
-    <!-- The H1 + main image Section -->
-    <h2>Login</h2>
-
-    <form method="post">
-    
-      <div class="container">
-        <label for="uname"><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="username" required>
-    
-        <label for="psw"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="password" required>
-        <a href="/logout.php"> logout </a>
-        <input type="submit" name="submit">
-      </div>
-      </div>
-    </form>
-
-
-
-<!-- The Footer Section -->
-<footer>
-  <p>Powered by me</p>
-</footer>
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
 <?php
+session_start();
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "organic_grocery_shop";
 
 //Login part
-include("db.connection.php");
+include("connection.php");
 
-if(isset($_POST["submit"])) {
-  $user=($_POST["username"]);
-  $sql = "SELECT password FROM users WHERE username='$user'";
-  $result = $conn->query($sql);
-  $found=0;
-  if ($result->num_rows > 0) {
-      // output data of each row
-      while($row = $result->fetch_assoc()) {
-          if($row["password"]==$_POST["password"])
-      {
-        echo "Ok";
-        $found=1;
-        $_SESSION["username"] = $user;
-        break;
-      }
-      }
-    if($found==0)
-    {
-      echo "Bad";
-    }
-}
+if(isset($_POST['submit'])) {
+	$user = mysqli_real_escape_string($mysqli, $_POST['username']);
+	$pass = mysqli_real_escape_string($mysqli, $_POST['password']);
 
-		if(isset($_SESSION["username"])) {
-			header('Location: index.html');			
+	if($user == "" || $pass == "") {
+		echo "Either username or password field is empty.";
+		echo "<br/>";
+		echo "<a href='login.php'>Go back</a>";
+	} else {
+		$result = mysqli_query($mysqli, "SELECT * FROM login WHERE username='$user' AND password=md5('$pass')")
+					or die("Could not execute the select query.");
+		
+		$row = mysqli_fetch_assoc($result);
+		
+		if(is_array($row) && !empty($row)) {
+			$validuser = $row['username'];
+			$_SESSION['valid'] = $validuser;
+			$_SESSION['name'] = $row['name'];
+			$_SESSION['id'] = $row['id'];
+
+		} else {
+			echo "Invalid username or password.";
+			echo "<br/>";
+			echo "<a href='login.php'>Go back</a>";
+		}
+
+		if(isset($_SESSION['valid'])) {
+			header('Location: index.php');			
 		}
 	}
- 
-
-include("end.connection.php");
+}       
+        
+        else {
+?>
+	<p><font size="+2">Login</font></p>
+	<form name="form1" method="post" action="">
+		<table width="75%" border="0">
+			<tr> 
+				<td width="10%">Username</td>
+				<td><input type="text" name="username"></td>
+			</tr>
+			<tr> 
+				<td>Password</td>
+				<td><input type="password" name="password"></td>
+			</tr>
+			<tr> 
+				<td>&nbsp;</td>
+				<td><input type="submit" name="submit" value="Submit"></td>
+			</tr>
+		</table>
+	</form>
+<?php
+}
 ?>
 </body>
 </html>
