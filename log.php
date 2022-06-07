53d4c4c7d7f23b6cf91200e9ebc10d11dac0597b
Tawfiak22 <79513425+Tawfiak22@users.noreply.github.com>
2022-06-07 20:14:11 +0100

    log php

--- log.php
+++ log.php
@@ -14,12 +14,12 @@
   <body>
     <nav>
       <ul class="topnav">
-      <li><a class="active" href="index.php">HOME</a></li>
-        <li><a href="about.php">ABOUT</a></li>
-        <li><a href="product.php">PRODUCT</a></li>
-        <li><a href="faq.php">FAQ</a></li>
-        <li><a href="contact.php">CONTACT</a></li>
-        <li><a href="reg.php">SING UP</a></li>
+        <li><a class="active" href="index.html">HOME</a></li>
+        <li><a href="about.html">ABOUT</a></li>
+        <li><a href="product.html">PRODUCT</a></li>
+        <li><a href="faq.html">FAQ</a></li>
+        <li><a href="contact.html">CONTACT</a></li>
+        <li><a href="reg.html">SING UP</a></li>
         <li><a href="log.php">LOG IN</a></li>
         <li><a href="order.php">ORDER</a></li>
       </ul>
@@ -27,27 +27,61 @@
 
     <!-- The H1 + main image Section -->
     <h2>Login</h2>
+
+    <form method="post">
     
       <div class="container">
         <label for="uname"><b>Username</b></label>
-        <input type="text" placeholder="Enter Username" name="uname" required>
+        <input type="text" placeholder="Enter Username" name="username" required>
     
         <label for="psw"><b>Password</b></label>
-        <input type="password" placeholder="Enter Password" name="psw" required>
+        <input type="password" placeholder="Enter Password" name="password" required>
         <a href="/logout.php"> logout </a>
-        <button type="submit">Login</button>
+        <input type="submit" name="submit">
       </div>
       </div>
     </form>
 
-  </body>
 
-<body>
 
 <!-- The Footer Section -->
 <footer>
   <p>Powered by me</p>
 </footer>
+<?php
 
+//Login part
+include("db.connection.php");
+
+if(isset($_POST["submit"])) {
+  $user=($_POST["username"]);
+  $sql = "SELECT password FROM users WHERE username='$user'";
+  $result = $conn->query($sql);
+  $found=0;
+  if ($result->num_rows > 0) {
+      // output data of each row
+      while($row = $result->fetch_assoc()) {
+          if($row["password"]==$_POST["password"])
+      {
+        echo "Ok";
+        $found=1;
+        $_SESSION["username"] = $user;
+        break;
+      }
+      }
+    if($found==0)
+    {
+      echo "Bad";
+    }
+}
+
+		if(isset($_SESSION["username"])) {
+			header('Location: index.html');			
+		}
+	}
+ 
+
+include("end.connection.php");
+?>
 </body>
 </html>
