486506317d4af6fc8272ee5aedb63483433a5914
Tawfiak22 <79513425+Tawfiak22@users.noreply.github.com>
2022-06-07 22:54:23 +0100

    delete php

--- delete.php
+++ delete.php
@@ -1,14 +1,16 @@
 <html>
 <body>
 <?php
+
 if(isset($_SESSION["username"])){
     $result = $conn->query("DELETE FROM users WHERE username='$_SESSION[username]'");
         echo "okay";
+}
 
-else{
+else {
         echo "not okay";
     }
-}
+
 ?>
 </body>
 </html>
